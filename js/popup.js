b57641006c3d21dc9734896dd5483c5cdec61dbc
ishida-shunya <t200d005@gunma-u.ac.jp>
2023-07-14 14:42:06 +0900

    UIの調整, ソースの整形

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    // element of checkbox ,text input, button and scroll button
+    /*element of checkbox ,text input, button, scroll button, and scroll target*/
     const chcckbox_elements = document.querySelectorAll(`input[type="checkbox"].filterbtn`);
     const text_elements = document.querySelectorAll(`input[type="text"]`);
     const exportbtn = document.querySelector(`button[class="export"]`);
-    let left = document.querySelector('.scrollleft');
-    let right = document.querySelector('.scrollright');
+    let scrollleft  = document.querySelector('.scrollleft');
+    let scrollright = document.querySelector('.scrollright');
     let scrolltarget = document.querySelector('.flex.left');
 
-    const scroolloffset = 450; 
+    const scroolloffset = 462; 
+    let willscroll = 0;
     var csscode='';
     let domainname = '';
 
@@ -21,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
         list: ':is(div[style="position: sticky; top: var(--stickyTop, 0); z-index: 1000;"]:has(.x5oN2.xbw4c),header:has(.ti-list))~div ',
         role: ':is(.xbt7a:has(.ti-badge), div[style="position: sticky; top: var(--stickyTop, 0); z-index: 1000;"]:has(.ti-badge),header:has(.ti-badge))~div ',
         user: ':is(div[style="position: sticky; top: var(--stickyTop, 0); z-index: 1000;"]:has(.xy0IK), .xbt7a:has(.ti-user))~div ',
-        all: '',
+        all: ''
     }
     /*CSSs which select specfic note and other CSS*/
     const hidecss = {
@@ -39,16 +40,15 @@ document.addEventListener('DOMContentLoaded', function () {
         userrenotemute: '.xcSej.x3762:has(.xBwhh > a[href="/@unko"]){ display: none; }',
         userstatus: '.status a:nth-child(n+2)  b,.x1tDq .x33Tu:nth-child(n+2) div:nth-child(2), .xyEEg .x8w8X:nth-child(n+2) span{font-size: 0;}.status a:nth-child(n+2)  b:after,.x1tDq .x33Tu:nth-child(n+2) div:nth-child(2):after, .xyEEg .x8w8X:nth-child(n+2) span:after{ content: "???"; font-size: 14px;}',
         emojibetter: ':root {    --emoji_default_size: 30px;    --emoji_margin_lr: 6px;    --emoji_margin_tb: 2px;    --emoji_max_size: 80px;    --emoji_displey_style: fill;      --emoji_window_default_height: 400px;      --emoji_window_default_width: 400px;    --emoji_autofill_max_width:  300px;    --emoji_autofill_displey_style: fill;}.emojis {  padding:  10px 5px 5px;  text-align:center;}.emojis section>.body {  display: inline !important;  padding:  0 !important;  line-height: 0;}.emojis section>.body .item{  height: auto !important;}button:has(.emoji){  aspect-ratio: auto!important;  width: fit-content !important;  contain: layout !important;  margin: 0px !important;  padding: var(--emoji_margin_tb)  var(--emoji_margin_lr)  !important;  min-height:  var(--emoji_default_size) !important;}button:has(.emoji) img{  width: auto !important;  height: var(--emoji_default_size) !important;  object-fit: var(--emoji_displey_style) !important;  object-position: 0% 50%;  max-width: var(--emoji_max_size);} .xeJ4G.xuoKL, ._emoji_1pjrm_56 {  width: auto !important;  object-fit: var(--emoji_autofill_displey_style) !important;  object-position: 0% 50%;  max-width: var(--emoji_autofill_max_width);}'
-
     }
-    const transitioncode = "<style> * { transition: background-color .5s; } </style>"
     const exportcomment = '/*今のMisskey-TL-FIlterの設定と同一のフィルタリングができるカスタムCSSです。\nこのコードをコピーして、フィルタを設定したいパソコン、スマホのmisskeyの設定→全般→カスタムCSS の中に貼り付けてください*/\n';
 
 
-    /*create css code and save from now settings*/
+
+    /*create css code from current settings*/
     function CreateCSS(){
         csscode="";
-        //Judges which CSS to apply based on the unique attributes assigned to the HTML of the checkbox and generates code
+        //checkbox
         for (let target of chcckbox_elements) {
             if(target.checked){
                 csscode += (tlselector[target.dataset.tl] + hidecss[target.dataset.kinds]) + '\n';
@@ -65,18 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    /*Update CSS from arg (excute script)*/
-    function UpdateCSS(styles){
-        document.querySelector(`.filtercsswrapper`).innerHTML=styles;
-        localStorage.setItem('lastcss',styles);
-    }
-
-    /*Get page Domain (excute script)*/
-    function GetDomain(){
-        return document.domain;
-    }
-
-    /*save current setting in localstorage*/
+    /*save current settings*/
     function SaveSetting(){
         for (let target of chcckbox_elements) {
             localStorage.setItem('button-' + target.dataset.tl + '-' + target.dataset.kinds + domainname, target.checked? 1 : 0);
@@ -84,15 +73,16 @@ document.addEventListener('DOMContentLoaded', function () {
         for (let target of text_elements) {
             localStorage.setItem('list-' + target.dataset.kinds + domainname, target.value);
         }
-        /*this setting use service worker. but this dont access localstorage. so save to chrome storage API*/
+        //this setting use service worker. but this dont access localstorage. so save to chrome storage API
         chrome.storage.local.set({setting1: text_elements[2].value});
         localStorage.setItem('saved' + domainname , '1');
     }
 
-    /*load settings from localStorage*/
+    /*load settings*/
     function LoadSetting(){
-        /*when first time, previous setup don't exist, so nothing*/
+        //when first time, previous setup don't exist, so init setting
         if(!localStorage.getItem('saved' + domainname)) {
+            SaveSetting();
             return;
         }
         for (let target of chcckbox_elements) {
@@ -105,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    /*export css code*/
+    /*create css code file and download*/
     function ExportCSS(){
         CreateCSS();
         const blob = new Blob([exportcomment + csscode],{type:"text/plain"});
@@ -116,21 +106,58 @@ document.addEventListener('DOMContentLoaded', function () {
         link.remove();
     }
 
+
+
+    /*Update CSS from arg (excute script)*/
+    function UpdateCSS(styles){
+        document.querySelector(`.filtercsswrapper`).innerHTML=styles;
+        localStorage.setItem('lastcss',styles);
+    }
+
+    /*Get page Domain (excute script)*/
+    function GetDomain(){
+        return document.domain;
+    }
+    
+
+
+    function clamp(a,b,c){
+        if(a>b){
+            return a;
+        } else if(c<b){
+            return c;
+        } else {
+            return b;
+        }
+    }
+
+    /*calclate scroll position*/
+    function setScrollPosition(flag){
+       var targetwidth = scrolltarget.scrollWidth - document.body.clientWidth;
+       if(flag) {
+        willscroll = clamp(0, willscroll+scroolloffset , targetwidth);
+       } else {
+        willscroll = clamp(0, willscroll-scroolloffset , targetwidth);
+       }
+    }
+
     /*scroll button event*/
-    left.addEventListener(`click`, () => {
-        var targetpositon = scrolltarget.scrollLeft;
+    scrollleft.addEventListener(`click`, () => {
+        setScrollPosition(false);
         scrolltarget.scrollTo({
-            left: targetpositon - scroolloffset,
+            left: willscroll,
             behavior: 'smooth'
           });
     })
-    right.addEventListener(`click`, () => {
-        var targetpositon = scrolltarget.scrollLeft;
+    scrollright.addEventListener(`click`, () => {
+        setScrollPosition(true);
         scrolltarget.scrollTo({
-            left: targetpositon + scroolloffset,
+            left: willscroll,
             behavior: 'smooth'
           });
     })
+
+
 
     /*Get the domain of tab which currently open 
     & load setting for current domain*/
@@ -147,7 +174,7 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(err => alert("設定の読み込みでエラーが発生しました、再読み込みしてみてね"));
     });
 
-    //Set　event listeners to rewrite　CSS when checkbox and textarea are changed
+    /*Set　event listeners*/
     for (let target of chcckbox_elements) {
     target.addEventListener(`change`, () => {
         CreateCSS();
@@ -174,11 +201,15 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         })
     }
+
+
+    
     /*set export button event*/
     exportbtn.addEventListener('click', () => {
         ExportCSS();
     })
 
+    const transitioncode = "<style> * { transition: background-color .5s; } </style>"
     setTimeout(function(){ 
         document.querySelector(`head`).insertAdjacentHTML('beforeend', transitioncode)
     },500);
